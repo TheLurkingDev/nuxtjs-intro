3519554ef1d9c55db6e3d7384f00055a2366e9ba
TheLurkingDev <thelurkingdev@gmail.com>
2018-08-19 20:19:02 -0400

    Added pass-through to single recipe.

--- recipes/pages/recipes/index.vue
+++ recipes/pages/recipes/index.vue
@@ -3,11 +3,13 @@
         <Recipe
             thumbnail="https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.foodandwine.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2F1519844002%2Ffast-food-mobile-apps-chick-fil-a-FT-BLOG0218.jpg%3Fitok%3D7d_gu0JA&w=800&q=85"
             title="Chic Fil' A"
+            id="1"
             previewText="Delicious chicken sandwich."
         />
         <Recipe
             thumbnail="https://amp.businessinsider.com/images/5a7dc169d03072af008b4bf2-750-562.jpg"
             title="Vege-burger"
+            id="2"
             previewText="From Red Robin no less."
         />        
     </section>
