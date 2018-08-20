422d618692a0e8fcb52dd91d0ca241035af9567c
TheLurkingDev <thelurkingdev@gmail.com>
2018-08-19 21:41:13 -0400

    Using asyncData() with a promise to return an object that mimics retrieving data from a db.

--- recipes/pages/recipes/index.vue
+++ recipes/pages/recipes/index.vue
@@ -1,17 +1,13 @@
 <template>
     <section class="recipes">
-        <Recipe
-            thumbnail="https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.foodandwine.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2F1519844002%2Ffast-food-mobile-apps-chick-fil-a-FT-BLOG0218.jpg%3Fitok%3D7d_gu0JA&w=800&q=85"
-            title="Chic Fil' A"
-            id="1"
-            previewText="Delicious chicken sandwich."
+        <Recipe 
+            v-for="recipe in recipes"
+            :key="recipe.id"
+            :thumbnail="recipe.thumbnail"
+            :title="recipe.title"
+            :id="recipe.id"
+            :previewText="recipe.previewText"
         />
-        <Recipe
-            thumbnail="https://amp.businessinsider.com/images/5a7dc169d03072af008b4bf2-750-562.jpg"
-            title="Vege-burger"
-            id="2"
-            previewText="From Red Robin no less."
-        />        
     </section>
 </template>
 
@@ -21,6 +17,29 @@ import Recipe from '@/components/Recipe';
 export default {
     components: { // Register the Recipe component.
         Recipe
+    },
+    asyncData() { // Loaded from server on initial fetch.
+        // Could use an axios call here: return axios.get()
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve({
+                    recipes: [
+                        {
+                            id: "1",
+                            title: "Chic Fil' A",
+                            previewText: "Delicious chicken sandwich.",
+                            thumbnail: "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.foodandwine.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2F1519844002%2Ffast-food-mobile-apps-chick-fil-a-FT-BLOG0218.jpg%3Fitok%3D7d_gu0JA&w=800&q=85"
+                        },
+                        {
+                            id: "2",
+                            title: "Vege-burger",
+                            previewText: "From Red Robin no less.",
+                            thumbnail: "https://amp.businessinsider.com/images/5a7dc169d03072af008b4bf2-750-562.jpg"
+                        }
+                    ]
+                })
+            }, 1500);
+        });
     }
 }
 </script>
